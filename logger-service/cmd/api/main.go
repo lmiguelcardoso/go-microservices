9c33fade523c888ac6862e5b4a0525d76fd7d7a0
Miguel <mi_lmc@hotmail.com>
2024-12-23 10:06:44 -0300

    feat: implement crud logs in data model

--- logger-service/cmd/api/main.go
+++ logger-service/cmd/api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"log"
+	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -29,6 +30,16 @@ func main() {
 	}
 
 	client = mongoClient
+
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+
+	defer cancel()
+
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
 }
 
 func connectToMongo() (*mongo.Client, error) {
