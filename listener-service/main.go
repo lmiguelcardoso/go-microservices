989e808418eb53b59645637f6817ff035d73c588
Miguel <mi_lmc@hotmail.com>
2025-01-02 10:48:15 -0300

    feat: setting up consumer configs in main

--- listener-service/main.go
+++ listener-service/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"listener/event"
 	"log"
 	"math"
 	"os"
@@ -25,18 +26,26 @@ func main() {
 		os.Exit(1)
 	}
 	defer rabbitConn.Close()
-	// start listening for message
 
 	// create consumer
+	consumer, err := event.NewConsumer(rabbitConn)
+	if err != nil {
+		panic(err)
+	}
 
 	// watch the queue and consume events
+	err = consumer.Listen([]string{"log.INFO", "log.WARNING", "log.ERROR"})
+
+	if err != nil {
+		log.Println(err)
+	}
 }
 
 func connect() (*amqp.Connection, error) {
 	var connection *amqp.Connection
 
 	for attempt := 1; attempt <= maxRetries; attempt++ {
-		c, err := amqp.Dial("amqp://guest:guest@localhost")
+		c, err := amqp.Dial("amqp://guest:guest@rabbitmq")
 		if err != nil {
 			log.Printf("Attempt %d: RabbitMQ not ready. Retrying in %v...", attempt, initialBackoff)
 		} else {
